7a9eb93424850d082f9f3cc9e7c7cd1e1b2d85ce
pugazhenthi.r <pugazhenthi.r@acldigital.com>
2023-11-03 14:38:54 +0530

    Sample Commit

---
+++ dirbrowse.php
@@ -0,0 +1,10 @@
+<?php 
+/*if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) 
+{
+    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+    die( header( 'location: /error.php' ) );
+}*/
+?>
+<video width="100%" controls>
+  <source src="videos/BU Head.mp4" type="video/mp4">
+</video>
